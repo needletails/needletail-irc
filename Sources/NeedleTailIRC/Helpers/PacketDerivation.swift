e45c35b720bd0d3782a28277f42a23dea6dd2da0
Cole Moore <Cartisim@icloud.com>
2024-12-12 10:59:59 +0800

    removed BSON Decoder extension

--- Sources/NeedleTailIRC/Helpers/PacketDerivation.swift
+++ Sources/NeedleTailIRC/Helpers/PacketDerivation.swift
@@ -415,32 +415,32 @@ public struct IRCMessageGenerator: Sendable {
     }
 }
 
-
-
-extension BSONDecoder {
-    enum Errors: Error, Sendable {
-        case nilData
-    }
-    public func decodeString<T: Codable>(_ type: T.Type, from string: String) throws -> T {
-        guard let data = Data(base64Encoded: string) else { throw Errors.nilData }
-        return try decode(type, from: Document(data: data))
-    }
-
-    public func decodeData<T: Codable>(_ type: T.Type, from data: Data) throws -> T {
-        return try decode(type, from: Document(data: data))
-    }
-
-    public func decodeBuffer<T: Codable>(_ type: T.Type, from buffer: ByteBuffer) throws -> T {
-        return try decode(type, from: Document(buffer: buffer))
-    }
-}
-
-extension BSONEncoder {
-    public func encodeString<T: Codable>(_ encodable: T) throws -> String {
-        try encode(encodable).makeData().base64EncodedString()
-    }
-
-    public func encodeData<T: Codable>(_ encodable: T) throws -> Data {
-        try encode(encodable).makeData()
-    }
-}
+//
+//
+//extension BSONDecoder {
+//    enum Errors: Error, Sendable {
+//        case nilData
+//    }
+//    public func decodeString<T: Codable>(_ type: T.Type, from string: String) throws -> T {
+//        guard let data = Data(base64Encoded: string) else { throw Errors.nilData }
+//        return try decode(type, from: Document(data: data))
+//    }
+//
+//    public func decodeData<T: Codable>(_ type: T.Type, from data: Data) throws -> T {
+//        return try decode(type, from: Document(data: data))
+//    }
+//
+//    public func decodeBuffer<T: Codable>(_ type: T.Type, from buffer: ByteBuffer) throws -> T {
+//        return try decode(type, from: Document(buffer: buffer))
+//    }
+//}
+//
+//extension BSONEncoder {
+//    public func encodeString<T: Codable>(_ encodable: T) throws -> String {
+//        try encode(encodable).makeData().base64EncodedString()
+//    }
+//
+//    public func encodeData<T: Codable>(_ encodable: T) throws -> Data {
+//        try encode(encodable).makeData()
+//    }
+//}
